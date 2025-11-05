8bea4c304fedef83e698bf4e4ea59be5a1f4a540
Szaski <szaski206@gmail.com>
2025-11-05 12:13:20 +0100

    test

--- Zadania1/src/Main.java
+++ Zadania1/src/Main.java
@@ -11,5 +11,6 @@ public class Main {
         //Fahrenheita = 1.8 * stopnieCelsjusza + 32.0
         stopnieFahrenheita = 1.8 * stopnieCelcjusza + 32;
         System.out.println("Stopnie Fahrenheita: " + stopnieFahrenheita);
+        // test
     }
 }
